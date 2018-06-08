c578544f9f821ec3bca40fb4e63a990ce3ac446e
cowmax <jensengliu@gmail.com>
2018-06-09 01:01:34 +0800

    it is the first version. it has been implemented following functions: 1. find document files e.g.  .doc, .docx, .xls, .xlsx etc. 2. compare document file size and modified time with its backup (file) , decide if the document file needs backup.

---
+++ AutoSaveDaemon/BackupConfig.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoSaveDaemon
+{
+    class BackupConfig
+    {
+        Dictionary<string, string> tag2DirMap = new Dictionary<string, string>();
+        List<string> backupFileTypes = new List<string>();
+
+        private string sourceDir = null;
+        private string defaultBakDir = null;
+
+        public BackupConfig(string backupDir)
+        {
+            this.defaultBakDir = backupDir;
+        }
+
+        public string SourceDir
+        {
+            get
+            {
+                return sourceDir;
+            }
+
+            set
+            {
+                sourceDir = value;
+            }
+        }
+
+        public string DefaultBakDir
+        {
+            get
+            {
+                return defaultBakDir;
+            }
+
+            set
+            {
+                defaultBakDir = value;
+            }
+        }
+
+        public bool addBackupFileTypes(string[] filetypes)
+        {
+            int n = 0;
+
+            foreach(string filetype in filetypes)
+            {
+                string extype = findBackupFileType(filetype);
+                if (extype == null)
+                {
+                    backupFileTypes.Add(filetype);
+                    n++;
+                }
+            }
+
+            return (n == filetypes.Length);
+        }
+
+        public bool addBackupFileType(string filetype)
+        {
+            bool succ = false;
+
+            string extype = findBackupFileType(filetype);
+            if (extype == null)
+            {
+                backupFileTypes.Add(filetype);
+                succ = true;
+            }
+            return succ;
+        }
+
+        public string findBackupFileType(string filetype)
+        {
+            string extype = null;
+            foreach(string t in this.backupFileTypes)
+            {
+                if (isMatchFileType(t, filetype))
+                {
+                    extype = filetype;
+                    break;
+                }
+            }
+            return extype;
+        }
+
+        public bool isBackupFileType(string filetype)
+        {
+            bool found = false;
+
+            foreach(string t in this.backupFileTypes)
+            {
+                if (isMatchFileType(t, filetype))
+                {
+                    found = true;
+                    break;
+                }
+            }
+            return found;
+        }
+
+        public static bool isMatchFileType(string type1, string type2)
+        {
+            bool rsl = false;
+            char[] cc = new char[] { '.', ' ' };
+            string ty1 = type1.ToLower();
+            string ty2 = type2.ToLower();
+             
+            if (ty1 == ty2)
+            {
+                rsl = true;
+            }
+            else if (ty1.Trim(cc) == ty2.Trim(cc))
+            {
+                rsl = true;
+            }
+            else if (ty1.IndexOf(ty2)>0|| ty2.IndexOf(ty1)>0)
+            {
+                rsl = true;
+            }
+            return rsl;
+        }
+
+
+        public string makeBackupFilePath(string srcfilepath)
+        {
+            string bakfilepath = null;
+            string dstDir = null;
+            string srcfilename = Path.GetFileName(srcfilepath);
+            string tag = getTagInFilename(srcfilename);
+
+            if (tag == null)
+            {
+                dstDir = this.defaultBakDir;
+            }
+            else
+            {
+                dstDir = this.tag2DirMap[tag];
+            }
+            
+            if (!Directory.Exists(dstDir))
+            {
+                Directory.CreateDirectory(dstDir);
+            }
+
+            bakfilepath = dstDir + "/" + srcfilename;
+
+            return bakfilepath;
+        }
+
+        public string getTagInFilename(string filename)
+        {
+            string tag = null;
+            string fname = filename.ToLower();
+            
+            foreach(string t in tag2DirMap.Keys)
+            {
+                if (fname.IndexOf(t) >= 0)
+                {
+                    tag = t;
+                    break;
+                }
+            }
+
+            return tag;
+        }
+
+        private string findBackDirByTag(string tag)
+        {
+            string dir = null;
+
+            if (tag2DirMap.ContainsKey(tag))
+            {
+                dir = tag2DirMap[tag];
+            }
+            return dir;
+        }
+
+        public bool addBackupDir(string tag, string dir)
+        {
+            bool succ = false;
+
+            string exdir = findBackDirByTag(tag);
+            if (exdir == null)
+            {
+                tag2DirMap.Add(tag, dir);
+                succ = true;
+            }
+
+            return succ;
+        }
+    }
+}
---
+++ AutoSaveDaemon/Program.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoSaveDaemon
+{
+    class Program
+    {
+        static List<string> promptMessages = new List<string>();
+        static BackupConfig bakcfg = new BackupConfig("C:/Users/Jenseng/OneDrive/有推/BAK/");
+
+        static void xprintline(string fmtstr, params object[] prms)
+        {
+            promptMessages.Add(string.Format(fmtstr, prms));
+        }
+
+        static void xprint(string fmtstr, params object[] prms)
+        {
+            int i = promptMessages.Count;
+            if (i == 0)
+            {
+                promptMessages.Add(string.Format(fmtstr, prms));
+            }
+            else
+            {
+                promptMessages[i-1] += string.Format(fmtstr, prms);
+            }
+        }
+
+        // Print info onto console
+        static void printAll()
+        {
+            foreach (string msg in promptMessages)
+            {
+                Console.WriteLine(msg + "\n");
+            }
+
+            promptMessages.Clear();
+        }
+
+        static void Main(string[] args)
+        {
+            
+            bakcfg.addBackupFileType(".doc");
+            bakcfg.addBackupFileType(".docx");
+            bakcfg.addBackupFileType(".xls");
+            bakcfg.addBackupFileType(".xlsx");
+            bakcfg.addBackupFileType(".ppt");
+            bakcfg.addBackupFileType(".pptx");
+            bakcfg.addBackupFileType(".vsd");
+            bakcfg.addBackupFileType(".vsdx");
+            bakcfg.addBackupFileType(".mmap");
+
+            bakcfg.SourceDir = "C:/Users/Jenseng/Desktop";
+            bakcfg.addBackupDir("xin", "C:/Users/Jenseng/OneDrive/有推/BAK/XIN");
+            bakcfg.addBackupDir("yot", "C:/Users/Jenseng/OneDrive/有推/BAK/YOT");
+
+            string[] allFiles = scanSourceDir(bakcfg.SourceDir);
+            xprint("{0} files was found.", allFiles.Length);
+
+            List<string> srcFiles = getNeedBackupFiles(allFiles);
+            xprint(" {0} document.", srcFiles.Count);
+
+            int n = backupFiles(srcFiles);
+
+            printAll();
+            Console.ReadKey();
+
+        }
+
+        private static int backupFiles(List<string> srcFiles)
+        {
+            int n = 0;
+            foreach(string srcfile in srcFiles)
+            {
+                string dstfile = bakcfg.makeBackupFilePath(srcfile);
+                FileInfo srcfileinfo = new FileInfo(srcfile);
+                FileInfo dstfileinfo = new FileInfo(dstfile);
+
+                if (isNeedToBackup(srcfileinfo, dstfileinfo))
+                {
+                    try
+                    {
+                        srcfileinfo.CopyTo(dstfile, true);
+                        xprintline("BACKUP [{0}] TO [{1}] COMPLETED.", srcfileinfo.Name, bakcfg.getTagInFilename(srcfileinfo.Name));
+                    }
+                    catch(IOException ex)
+                    {
+                        xprintline("FAILD to backup {0}", srcfileinfo.Name);
+                    }
+                }
+            }
+
+            return n;
+        }
+
+        private static bool isNeedToBackup(FileInfo srcfileinfo, FileInfo dstfileinfo)
+        {
+            int sz = 0;
+            int tm = -1;
+            if (dstfileinfo.Exists)
+            {
+                xprintline("[{0}] has been backuped before.", srcfileinfo.Name);
+                TimeSpan tsp = srcfileinfo.LastWriteTime - dstfileinfo.LastWriteTime;
+                tm = tsp.Seconds;
+                if (tm == 0)
+                {
+                    xprintline("[{0}] has NO CHANGE.", srcfileinfo.Name);
+                }
+                else if (tm > 0)
+                {
+                    xprintline("[{0}] has BEEN MODIFIED {1} sec. after last backup.", srcfileinfo.Name, tm);
+                }
+                else
+                {
+                    xprintline("[{0}]'s backup file IS NEWER {1} sec.", srcfileinfo.Name, tm);
+                }
+
+                sz = (int)(srcfileinfo.Length - dstfileinfo.Length);
+
+                if (sz == 0)
+                {
+                    xprintline("[{0}] has NO SIZE CHANGE", srcfileinfo.Name, sz);
+                }
+                else if(sz > 0)
+                {
+                    xprintline("[{0}] has BEEN INCREASED {1} bytes.", srcfileinfo.Name, sz);
+                }
+                else
+                {
+                    xprintline("[{0}] has BEEN DECREASED {1} bytes.", srcfileinfo.Name, sz);
+                }
+                
+            }
+            else
+            {
+                xprintline("[{0}] has never been backuped.", srcfileinfo.Name);
+            }
+
+            return (!dstfileinfo.Exists) || (sz != 0 && tm > 0);
+        }
+
+        private static List<string> getNeedBackupFiles(string[] allFiles)
+        {
+            List<string> bakfiles = new List<string>();
+            foreach(string f in allFiles)
+            {
+                string filetype = getFileType(f);
+                if (bakcfg.isBackupFileType(filetype))
+                {
+                    bakfiles.Add(f);
+                }
+            }
+            return bakfiles;
+        }
+
+        private static string getFileType(string filepath)
+        {
+            return Path.GetExtension(filepath);
+        }
+
+        static string[] scanSourceDir(string srcDir)
+        {
+            string[] allfiles = null;
+
+            if (!Directory.Exists(srcDir))
+            {
+                xprintline("Directory ({0}) does not exist!", srcDir);
+            }
+            else
+            {
+                 allfiles = Directory.GetFiles(srcDir, "*", SearchOption.TopDirectoryOnly);
+            }
+
+            return allfiles;
+        }
+    }
+}
